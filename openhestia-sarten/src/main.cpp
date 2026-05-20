53691462a5901e86cb2c58c2e6fb0c60d0d2ffa5
jabg5-ua <jabg5@alu.ua.es>
2026-05-20 11:27:21 +0200

    cargados datos acelerometro

--- openhestia-sarten/src/main.cpp
+++ openhestia-sarten/src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
 #include <math.h>
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <jabg5-ua-project-1_inferencing.h>
+
+//direccion de acelerometro
+Adafruit_MPU6050 mpu;
+const int MPU_ADDR = 0x68;
 
 // ── Pines mux ─────────────────────────────────────────────────
 #define MUX_S0 1
@@ -88,6 +96,47 @@ void taskTemperatura(void *pvParameters)
     }
 }
 
+void taskInferencia(void *pvParameters) {
+    while (true) {
+        // 1. Crear el buffer para la IA
+        float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
+
+        // 2. Llenar el buffer a la frecuencia exacta que pide Edge Impulse
+        for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += 3) {
+            uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
+            
+            sensors_event_t a, g, temp;
+            mpu.getEvent(&a, &g, &temp);
+
+            buffer[ix + 0] = a.acceleration.x;
+            buffer[ix + 1] = a.acceleration.y;
+            buffer[ix + 2] = a.acceleration.z;
+
+            // Esperar al siguiente tick, pero cediendo tiempo al ESP32 (FreeRTOS)
+            while (micros() < next_tick) { 
+                taskYIELD(); 
+            }
+        }
+
+        // 3. Convertir buffer y ejecutar clasificador
+        signal_t signal;
+        numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+        
+        ei_impulse_result_t result = { 0 };
+        EI_IMPULSE_ERROR r = run_classifier(&signal, &result, false);
+        
+        if (r == EI_IMPULSE_OK) {
+            Serial.println("=== PREDICCIÓN SARTÉN ===");
+            for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+                Serial.printf("  %s: %.2f\n", result.classification[ix].label, result.classification[ix].value);
+            }
+        }
+        
+        // Pausa entre inferencias para no saturar
+        vTaskDelay(pdMS_TO_TICKS(500)); 
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
@@ -99,6 +148,26 @@ void setup()
     analogReadResolution(12);
 
     xTaskCreate(taskTemperatura, "TareaTemp", 4096, NULL, 1, NULL);
+
+    // Iniciar bus I2C
+    Wire.begin(8, 9);
+
+    // Despertar manual del MPU6050
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x6B); 
+    Wire.write(0);    
+    Wire.endTransmission(true);
+    vTaskDelay(pdMS_TO_TICKS(100)); // En FreeRTOS usamos vTaskDelay en lugar de delay()
+
+    if (mpu.begin(MPU_ADDR, &Wire, 0)) {
+        mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+        mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    } else {
+        Serial.println("¡Error al iniciar el MPU6050!");
+    }
+
+    xTaskCreate(taskInferencia, "TareaIA", 16384, NULL, 1, NULL);
+
 }
 
 void loop() {}
